8ca36f337799fad71838ba7e756b115733de5fb6
PacksGamingHD <craftyswans@gmail.com>
2015-02-12 11:22:52 -0800

    Mass update

--- CrystalOS/CrystalOS.ino
+++ CrystalOS/CrystalOS.ino
@@ -18,80 +18,88 @@ const int button2Pin = 8; // This is button 2
 const int temperaturePin = 0; // This is the pin the tempature sensor uses
 void start() // This delays two seconds, set the cursor to the first line and clears the LCD
 {
-  delay(2000);
-  lcd.setCursor(0,0);
-  lcd.clear();
+  delay(2000); // Delay 2000
+  lcd.setCursor(0,0); // Set cursor to first line
+  lcd.clear(); // Clear LCD
 }
 void start1() // Same as above but it sets the cursor to the second line
 {
-  delay(2000);
+  delay(2000); // Delay 2000
+  lcd.setCursor(0,1); // Set cursor to second line
+  lcd.clear(); // Clear LCD
+}
+void boot1() // Delay 1500 then set LCD to first line
+{
+  delay(1500); // Delay 1500
+  lcd.setCursor(0,0); // Set cursor to first line
+}
+void line1();
+{
+  lcd.setCursor(0,0);
+}
+void line2()
+{
   lcd.setCursor(0,1);
-  lcd.clear();
 }
 void setup() // Setup code
 {
-pinMode(button1Pin, INPUT);
-  lcd.begin(16, 2);
-  lcd.clear();
-  Serial.begin(9600);
-  Serial.begin(14400);
-  lcd.setCursor(0,0);
+pinMode(button1Pin, INPUT); // Button 1
+pinMode(button2Pin, INPUT); // Button 2
+  lcd.begin(16, 2); // 16 charachters on 2 lines
+  lcd.clear(); // Clear LCD
+  lcd.setCursor(0,0); // Set cursor to first line
   // Below is the code that displays when you plug it in or reset the Arduino
-lcd.print("O");
-lcd.setCursor(0,1);
-lcd.print("Starting up");
-delay(1500);
-lcd.setCursor(0,0);
-lcd.print("O O");
-lcd.setCursor(0,1);
-lcd.print("Loading /home");
-delay(1500);
-lcd.setCursor(0,0);
-lcd.print("O O O");
-lcd.setCursor(0,1);
-lcd.print("Loading /temp");
-delay(1500);
-lcd.setCursor(0,0);
-lcd.clear();
-lcd.print("O O O O O O");
-lcd.setCursor(0,1);
-lcd.print("Loading /txt");
-delay(1500);
-lcd.setCursor(0,0);
-lcd.print("O O O O O O O O");
-lcd.setCursor(0,1);
-lcd.print("Loading /buttons");
-delay(2500);
-lcd.clear();
+lcd.print("O"); // Print to LCD
+lcd.setCursor(0,1); // Set cursor to second line
+lcd.print("Starting up"); // Print to LCD
+boot1(); // Run function boot1
+lcd.print("O O"); // Print to LCD
+line2(); // Set cursor to second line
+lcd.print("Loading /home"); // Print to LCD
+boot1(); // Run function boot1
+lcd.print("O O O"); // Print to LCD
+line2();// Run function line2
+lcd.print("Loading /temp"); // Print to LCD
+boot1(); // Run function boot1
+lcd.clear(); // Clear LCD
+lcd.print("O O O O O O"); // Print to LCD
+line2(); // Run function line2
+lcd.print("Loading /txt"); // Print to LCD
+boot1(); // Run function boot1
+lcd.print("O O O O O O O O"); // Print to LCD
+lien2(); // Run function line2
+lcd.print("Loading /buttons"); // Print to LCD
+delay(2500); // Delay 2500
+lcd.clear(); // Clear LCD
 }
 float getVoltage(int pin) // For the tempature sensor
 {
-  return (analogRead(pin) * 0.004882814);
+  return (analogRead(pin) * 0.004882814); // Analog read pin
 }
 void loop() // This is the main code
 {
-int button1State;
-button1State = digitalRead(button1Pin);
-int button2State;
-button2State = digitalRead(button2Pin);
-float voltage, degreesC, degreesF;
-voltage = getVoltage(temperaturePin);
-degreesC = (voltage - 0.5) * 100.0;
-degreesF = degreesC * (9.0/5.0) + 32.0;
-lcd.setCursor(0,0);
-lcd.print("CrystalOS - v3.0");
-lcd.setCursor(0,1);
-lcd.print("Blue = Tempature");
+int button1State; // Define button 1
+button1State = digitalRead(button1Pin); // Tell where button 1 is 
+int button2State; // Define button 2
+button2State = digitalRead(button2Pin); // Tell where button 2 is
+float voltage, degreesC, degreesF; // Define voltage, degreesC and degreesF
+voltage = getVoltage(temperaturePin); // Tells what voltage is
+degreesC = (voltage - 0.5) * 100.0; // Tells what the tempature can be
+degreesF = degreesC * (9.0/5.0) + 32.0; // Convert tempature
+line1(); // Run function line1
+lcd.print("CrystalOS - v3.0"); // Print to LCD
+line2(); // Run function line2
+lcd.print("Blue = Tempature"); // Print to LCD
 if (button1State == LOW) // If button 1 is pressed then tell the tempature
 {
-  lcd.setCursor(0,0);
-  lcd.clear();
-  lcd.print("Voltage ");
-  lcd.setCursor(0,1);
-  lcd.print(voltage);
-  start();
-  lcd.print("Celsius");
-  lcd.setCursor(0,1);
+  line1(); // Run function line1
+  lcd.clear(); // Clear LCD
+  lcd.print("Voltage "); // Print to LCD
+  line2(); // Run function line2
+  lcd.print(voltage); // Print voltage to LCD
+  start(); // Run function start
+  lcd.print("Celsius"); // Print to LCD
+  line2(); // Run function line2
   lcd.print(degreesC);
   start();
   lcd.print("Fahrenheit");
